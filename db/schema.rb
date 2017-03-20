69d3ad6375b97af2f7ffb3689d36125029709e65
Jonathan Rivers <jrrivers@gmail.com>
2017-03-20 01:38:57 +0000

    Edit devise reg views, tweek nav, add names to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319105519) do
+ActiveRecord::Schema.define(version: 20170319192130) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170319105519) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
